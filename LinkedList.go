83ec64df5d84562c119159888c72931c3c75e9b1
Nick McCrory <n.mccrory@icloud.com>
2017-03-10 15:24:12 -0800

    Created Doubly Linked List. Included tests. Currently tests append(data).

---
+++ LinkedList.go
@@ -0,0 +1,28 @@
+// I know it's called 'GolangAlgorithms' but this is a data structure. :P
+package GolangAlgorithms
+
+type Node struct {
+	data int
+	next, prev *Node
+}
+
+type List struct {
+	head *Node
+	tail *Node
+	count int
+}
+
+func (l *List) append(data int) *List {
+	n := &Node{data: data}
+	if (l.head == nil) {
+		l.head = n
+		l.count = 1
+	}else{
+		n.prev = l.tail
+		l.tail.next = n
+		l.count++
+	}
+	l.tail = n
+
+	return l
+}
---
+++ LinkedList_test.go
@@ -0,0 +1,17 @@
+package GolangAlgorithms
+
+import (
+	"testing"
+)
+
+func TestAppend(t *testing.T) {
+	list := &List{}
+	list.append(10)
+
+	if (list.count == 0) {
+		t.Fatalf("List is empty.")
+	}
+	if (list.head.data != 10) {
+		t.Fatalf("actual %d, want 10", list.head.data)
+	}
+}
